51bc8b6ed726382aba3ccee12fa583211209a36f
Wilkcraft <vallejogarijomiguel@gmail.com>
2026-05-23 18:01:43 +0200

    Auto sync Sat May 23 18:01:43 CEST 2026

--- formulas/formOr/aminasAmidasNitrilosNitro.js
+++ formulas/formOr/aminasAmidasNitrilosNitro.js
@@ -1,63 +1,72 @@
 export const formulas = [
   {
-    formula: "CH₃-CH₂-CONH₂",
-    answers: ["Propanamida"],
-    tipo: "amida",
+    "formula": "CH₃-CH₂-CONH₂",
+    "answers": [
+      "Propanamida"
+    ],
+    "tipo": "amida"
   },
-
   {
-    formula: "HCONH₂",
-    answers: ["Metanamida"],
-    tipo: "amida",
+    "formula": "HCONH₂",
+    "answers": [
+      "Metanamida"
+    ],
+    "tipo": "amida"
   },
-
   {
-    formula:
-      '<div style="display: flex; align-items: center; justify-content: center; gap: 10px;"><img src="../media/bencenamina.jpg" alt="../media/bencenamina.jpg" style="width: auto; height: 125px;" /></div>',
-    answers: ["Bencenamina"],
-    tipo: "amina",
+    "formula": "<div style=\"display: flex; align-items: center; justify-content: center; gap: 10px;\"><img src=\"../media/bencenamina.jpg\" alt=\"../media/bencenamina.jpg\" style=\"width: auto; height: 125px;\" /></div>",
+    "answers": [
+      "Bencenamina"
+    ],
+    "tipo": "amina"
   },
-
   {
-    formula: "CH₃-CH₂-NH₂",
-    answers: ["Etanamina"],
-    tipo: "amina",
+    "formula": "CH₃-CH₂-NH₂",
+    "answers": [
+      "Etanamina"
+    ],
+    "tipo": "amina"
   },
-
   {
-    formula: "CH₃-CH₂-CH₂-CH₂-CN",
-    answers: ["Pentanonitrilo"],
-    tipo: "nitrilo",
+    "formula": "CH₃-CH₂-CH₂-CH₂-CN",
+    "answers": [
+      "Pentanonitrilo"
+    ],
+    "tipo": "nitrilo"
   },
-
   {
-    formula: "CH₃-CH₂-CH₂-CN",
-    answers: ["Butanonitrilo"],
-    tipo: "nitrilo",
+    "formula": "CH₃-CH₂-CH₂-CN",
+    "answers": [
+      "Butanonitrilo"
+    ],
+    "tipo": "nitrilo"
   },
-
   {
-    formula:
-      '<div style="display: flex; align-items: center; justify-content: center; gap: 10px;"><img src="../media/nitrobenceno.jpg" alt="../media/nitrobenceno.jpg" style="width: auto; height: 125px;" /></div>',
-    answers: ["Nitrobenceno"],
-    tipo: "nitro",
+    "formula": "<div style=\"display: flex; align-items: center; justify-content: center; gap: 10px;\"><img src=\"../media/nitrobenceno.jpg\" alt=\"../media/nitrobenceno.jpg\" style=\"width: auto; height: 125px;\" /></div>",
+    "answers": [
+      "Nitrobenceno"
+    ],
+    "tipo": "nitro"
   },
-
   {
-    formula: "CH₂=CH-CH₂-NO₂",
-    answers: ["3-nitro-1-propeno"],
-    tipo: "nitro",
+    "formula": "CH₂=CH-CH₂-NO₂",
+    "answers": [
+      "3-nitro-1-propeno"
+    ],
+    "tipo": "nitro"
   },
-
   {
-    formula: "CH₃-CH₂-CN",
-    answers: ["Propanonitrilo"],
-    tipo: "nitrilo",
+    "formula": "CH₃-CH₂-CN",
+    "answers": [
+      "Propanonitrilo"
+    ],
+    "tipo": "nitrilo"
   },
-
   {
-    formula: "CH₃-NH-CH₂-CH₂-CH₂-CH₃",
-    answers: ["Metilbutilamina"],
-    tipo: "amina",
-  },
+    "formula": "CH₃-NH-CH₂-CH₂-CH₂-CH₃",
+    "answers": [
+      "Metilamina"
+    ],
+    "tipo": "amina"
+  }
 ];
