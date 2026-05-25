0158559464fa7d6541c9887e9e9250507db1b9d9
Wilkcraft <vallejogarijomiguel@gmail.com>
2026-05-25 19:39:42 +0200

    Auto sync Mon May 25 19:39:42 CEST 2026

--- server.js
+++ server.js
@@ -415,7 +415,7 @@ client.on("interactionCreate", async (interaction) => {
         let opciones = [];
 
         snapshot.forEach((doc) => {
-          const fechaID = doc.id;
+          const fechaID = doc.data().fechaID;
 
           // convertir dd_mm_aaaa → Date
           const [d, m, a] = fechaID.split("_");
