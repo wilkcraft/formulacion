371ff1740e72866be356943fd49e2cf4f6eba1e9
wilkcraft <vallejogarijomiguel@gmail.com>
2026-09-21 13:40:11 +0200

    backend Update

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const nodemailer = require("nodemailer");
 
 const admin = require("firebase-admin");
 const path = require("path");
-const serviceAccount = require(
-  path.join(__dirname, process.env.FIREBASE_KEY_PATH),
-);
+const serviceAccount = require(process.env.FIREBASE_KEY_PATH);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
